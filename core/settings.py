910b98fea0368af334265b6aff7f3f44978c93dd
“Santu <roysantu2002@yahoo.com>
2022-06-27 08:23:58 +0530

    added Heroku deployment

--- core/settings.py
+++ core/settings.py
@@ -1,6 +1,7 @@
 """
 """
 
+import os
 from datetime import timedelta
 from pathlib import Path
 
@@ -152,6 +153,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
